46890f6c4206596bb4c192fd82fc670991f87f80
thahzi <thahzeebmohamed@gmail.com>
2024-07-03 11:06:26 +0530

    july3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
 import './App.css'
+import Home from "./pages/Home";
+import Wishlist from './pages/Wishlist'
+import Cart from './pages/Cart'
+import { Route, Routes } from 'react-router-dom';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Routes>
+        <Route path='/'element={<Home/>}/>
+
+        <Route path='/wishlist'element={<Wishlist/>}/>
+
+        <Route path='/cart'element={<Cart/>}/>
+
+      </Routes>
     </>
   )
 }
